2ee4effad075fc57d25f2c4033896ecee597fba7
Valentin CHEVREAU <valentin.chevreau@hetic.net>
2018-09-25 10:29:10 +0200

    feat(webpack-react):create list of person with components

--- src/index.js
+++ src/index.js
@@ -1,10 +1,23 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+import List from './list'
+
 import './index.scss'
 
+const list = [{
+	name: 'Cyril',
+  age: 31
+}, {
+  name: 'Echo',
+  age: 24
+}, {
+  name: 'Valentin',
+  age: 21
+}]
+
 const App = () => (
-  <div>Hello React !</div>
+  <List list={list} />
 )
 
 ReactDOM.render(<App />, document.getElementById('app'))
